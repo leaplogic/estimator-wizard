76bf4ce8386bdf2fe5cefb876f521fe92f6be3f0
Adam Randlett <adam@randlett.net>
2020-02-21 14:37:59 -0700

    Add attributes to setting model

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -35,30 +35,17 @@ class Settings extends Model
     // =========================================================================
 
     /**
-     * Some field model attribute
+     * Email To
      *
      * @var string
      */
-    public $someAttribute = 'Some Default';
-
-    // Public Methods
-    // =========================================================================
+    public $emailTo;
 
     /**
-     * Returns the validation rules for attributes.
+     * Zipcodes
      *
-     * Validation rules are used by [[validate()]] to check if attribute values are valid.
-     * Child classes may override this method to declare different validation rules.
-     *
-     * More info: http://www.yiiframework.com/doc-2.0/guide-input-validation.html
-     *
-     * @return array
+     * @var string
      */
-    public function rules()
-    {
-        return [
-            ['someAttribute', 'string'],
-            ['someAttribute', 'default', 'value' => 'Some Default'],
-        ];
-    }
+    public $zipCodes;
+
 }
